92bffbf36870db6175b12a7630f267297a653b3e
JackBai2000 <j52bai@uwaterloo.ca>
2021-10-14 13:26:50 -0400

    Basic Movement
    Implemented Base Player Movement

---
+++ TempName/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        
+    }
+    // FixedUpdate updates every fixed interval, used for movement
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speed);
+    }
+}
